84d3f6fb7c242d5a61ca36a2c377eb05374f98fb
dylanlaborde <dylan4047@gmail.com>
2024-09-04 13:29:55 +0200

    add the banner componant

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div className='bannerContainer'>
+        <span>Chez vous, partout et ailleurs</span>        
+    </div>
+  )
+}
+
+export default Banner
