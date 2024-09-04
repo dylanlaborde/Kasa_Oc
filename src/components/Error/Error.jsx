6d9fd6be8343d5776961763ff222fd343cf912f5
dylanlaborde <dylan4047@gmail.com>
2024-09-04 13:37:28 +0200

    create the 404 page with this componant Error

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Error.css'
+
+function Error() {
+  return (
+    <div className='error-container'>
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <a href="/">Retourner sur la page d’accueil</a>  
+    </div>
+  )
+}
+
+export default Error
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import './NotFound.css'
+import Header from '../../components/Header/Header'
+import Error from '../../components/Error/Error'
+import Footer from '../../components/Footer/Footer'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='main'>
+      <Header/>
+      <Error/>
+      <Footer/>
+    </div>
+    
   )
 }
 
