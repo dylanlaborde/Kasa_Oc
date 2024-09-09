5c2d8dc87b3166a7c2e9d6a7ea6ed54c41c5f774
dylanlaborde <dylan4047@gmail.com>
2024-09-10 00:40:17 +0200

    remove warning by using useeffect and refactor document

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,37 +1,52 @@
-import React, { useState } from 'react'
-import './Carrousel.css'
-import ArrowLeft from '../../assets/arrow_back.png'
-import ArrowRight from '../../assets/arrow_forward.png'
+import React, { useEffect, useState } from "react";
+import "./Carrousel.css";
+import ArrowLeft from "../../assets/arrow_back.png";
+import ArrowRight from "../../assets/arrow_forward.png";
 
-export default function Carrousel({ pictures , pictureLenght}) {
+export default function Carrousel({ pictures, pictureLength }) {
   const [index, setIndex] = useState(0);
-  const [currentView,setCurentView]=useState(1)
-    if (currentView > pictureLenght) {
+  const [currentView, setCurentView] = useState(1);
+
+  useEffect(() => {
+    if (currentView > pictureLength) {
       setCurentView(1);
       setIndex(0);
     }
     if (currentView <= 0) {
-      setCurentView(pictureLenght);
-      setIndex(pictureLenght -1);
+      setCurentView(pictureLength);
+      setIndex(pictureLength - 1);
     }
+  }, [currentView, pictureLength]);
+
   function incrementView() {
-    setIndex(index + 1)
-    setCurentView(currentView + 1);
+    setIndex((prevIndex) => prevIndex + 1);
+    setCurentView((prevView) => prevView + 1);
   }
+
   function decrementView() {
-    setIndex(index - 1)
-    setCurentView(currentView - 1);
+    setIndex((prevIndex) => prevIndex - 1);
+    setCurentView((prevView) => prevView - 1);
   }
 
   return (
-    <section className='carrousel'>
-      <img src={ArrowLeft} onClick={decrementView} alt="left" className='arrows_left' />
-      <img src={ArrowRight} alt="right" className='arrows_right' onClick={incrementView}/>
-      <img src={pictures[index]} alt="image1" className='carrousel_img' />
-      <div className="viewWrapper" oneClick={incrementView}>
-        <span className='currentView'>{currentView}/</span>
-        <span className='viewLenght'>{pictureLenght}</span>
+    <section className="carrousel">
+      <img
+        src={ArrowLeft}
+        onClick={decrementView}
+        alt="left"
+        className="arrows_left"
+      />
+      <img
+        src={ArrowRight}
+        alt="right"
+        className="arrows_right"
+        onClick={incrementView}
+      />
+      <img src={pictures[index]} alt="image1" className="carrousel_img" />
+      <div className="viewWrapper" onClick={incrementView}>
+        <span className="currentView">{currentView}/</span>
+        <span className="viewLenght">{pictureLength}</span>
       </div>
     </section>
-  )
+  );
 }
