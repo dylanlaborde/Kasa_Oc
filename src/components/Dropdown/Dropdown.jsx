281b2d5ca9b854911d3c516ff51d77ac072ed08b
dylanlaborde <dylan4047@gmail.com>
2024-09-11 01:38:41 +0200

    refactor

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,17 +1,15 @@
-import React, { useState } from 'react'
-import chevron_up from '../../assets/chevron_up.png'
-import chevron_back from '../../assets/chevron_back.png'
+import React, { useState } from "react";
+import chevron_up from "../../assets/chevron_up.png";
+import chevron_back from "../../assets/chevron_back.png";
 
-import './Dropdown.css'
+import "./Dropdown.css";
 
 function Dropdown({ title, content }) {
-  const [isOpen, setisOpen] = useState(false)
+  const [isOpen, setisOpen] = useState(false);
   return (
     <>
-      {
-        isOpen ?
-          (
-            <div className="dropdown dropdown-open ">
+      {isOpen ? (
+        <div className="dropdown dropdown-open ">
           <div className="topbar">
             {title}
             <img
@@ -21,24 +19,17 @@ function Dropdown({ title, content }) {
             />
           </div>
           <div className="dropdownContent">{content}</div>
-            </div>
-          ) :
-          (
-            <div className="dropdown ">
-            <div className="topbar">
-              {title}
-              <img
-                src={chevron_up}
-                alt="back"
-                onClick={() => setisOpen(true)}
-              />
-            </div>
         </div>
-          )
-    }    
+      ) : (
+        <div className="dropdown ">
+          <div className="topbar">
+            {title}
+            <img src={chevron_up} alt="back" onClick={() => setisOpen(true)} />
+          </div>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default Dropdown
-
+export default Dropdown;
